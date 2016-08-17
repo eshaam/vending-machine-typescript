fbf702cd5556d68eab17010bc11368b3d1cfe7b5
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-08-17 10:04:42 +0200

    Accept coins and keep track at the vending machine

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -1,3 +1,10 @@
+/// <reference path="coin.ts" />
+
 class VendingMachine {
-    
+    //people will put coins into a machine so we will have to keep track of a total
+    //Its private because we will only use it internally
+    private paid = 0
+    acceptCoin = (coin: Quarter): void => {
+        this.paid = this.paid + coin.Value
+    }
 }
