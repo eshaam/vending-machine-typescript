41295995fd696bf7a4da6896970e394ddf49f98c
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-08-17 10:52:22 +0200

    Lets now insert coin with image and use array

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -4,6 +4,7 @@ class VendingMachine {
     //people will put coins into a machine so we will have to keep track of a total
     //Its private because we will only use it internally
     private paid = ko.observable(0)
+    acceptedCoins: Quarter[] = [new Quarter]
     acceptCoin = (coin: Quarter): void => {
         let oldTotal = this.paid()
         this.paid(oldTotal + coin.Value)
