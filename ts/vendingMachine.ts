a02b992db40e5e24f6508e31f20e9abdb29076c0
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-08-17 12:15:17 +0200

    Be able to select and pay for products

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -20,8 +20,10 @@ class VendingMachine {
     //people will put coins into a machine so we will have to keep track of a total
     //Its private because we will only use it internally
     private paid = ko.observable(0)
+    selectedCell = ko.observable(new Cell(new CocaCola))
     cells = ko.observableArray([])
     acceptedCoins: Quarter[] = [new Quarter]
+    canPay = ko.pureComputed( () => this.paid() - this.selectedCell().product.price >= 0)
 
     set size(givenSize: VendingMachineSize) {
         this.cells([])
@@ -30,8 +32,26 @@ class VendingMachine {
             this.cells.push(new Cell(product))
         }
     }
+
+    select = (cell: Cell) : void => {
+        cell.sold(false)
+        this.selectedCell(cell)
+    }
+
     acceptCoin = (coin: Quarter): void => {
         let oldTotal = this.paid()
         this.paid(oldTotal + coin.Value)
+    }
+
+    pay = () : void => {
+        if (this.selectedCell().stock() < 1) {
+            alert("I'm sorry we are out of them")
+            return
+        }
+        let currentPaid = this.paid()
+        this.paid(Math.round(((currentPaid - this.selectedCell().product.price) * 100))/ 100)
+        let currentStock = this.selectedCell().stock()
+        this.selectedCell().stock(currentStock - 1)
+        this.selectedCell().sold(true)
     }
 }
