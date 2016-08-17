d0e12bbedbe87c2ac499234696de82ac3a8e88e0
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-08-17 10:41:17 +0200

    refactor vm to use knock and also UI to use binding

--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -3,10 +3,9 @@
 class VendingMachine {
     //people will put coins into a machine so we will have to keep track of a total
     //Its private because we will only use it internally
-    private paid = 0
+    private paid = ko.observable(0)
     acceptCoin = (coin: Quarter): void => {
-        this.paid = this.paid + coin.Value
-        var element = document.getElementById("total")
-        element.innerHTML = this.paid.toString()
+        let oldTotal = this.paid()
+        this.paid(oldTotal + coin.Value)
     }
 }
